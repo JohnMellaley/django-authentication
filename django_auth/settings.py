976bda362dddc7c08155272505392bfb9b35e938
John Mellaley <mellaley@hotmail.com>
2019-05-31 14:04:01 +0000

    password reset

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -132,11 +132,10 @@ STATICFILES_DIRS = [
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 EMAIL_BACKEND ="django.core.mail.backends.console.EmailBackend"
-
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = 'johnmellaley@gmail.com'
+EMAIL_HOST_PASSWORD = 'Monaghan18*'
 EMAIL_PORT = 587
 
 
